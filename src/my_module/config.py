f68c5f88ef4cb29443c011c09d6496e85e343c42
Kai-Oshiro <kai-oshiro.cse@cat.hokudai.ac.jp>
2023-10-18 18:23:48 +0900

    add module

--- src/my_module/config.py
+++ src/my_module/config.py
@@ -1,21 +1,30 @@
 import configparser
 
-class ConfigProcessor: # Define class
-    def __init__(self, conf_file=None): # Define constructor
-        print(f"###Config file process###")
-        print(f"{conf_file} is referenced as a configuration file.\n")
+class ConfigProcessor:
+    def __init__(self, conf_file=None):
         self.config_ini = configparser.ConfigParser()
         self.config_ini.optionxform = str
         self.config_ini.read(conf_file)
 
+    # Read "JOB_DIR" section
     def read_job_dir(self):
-        # Read "JOB_DIR" section
         try:
-            dir_list = self.config_ini["JOB_DIR"]
+            job_dir = self.config_ini["JOB_DIR"]
+            dir_dict = dict(job_dir)
         except:
-            print(f"\"JOB_DIR\" section does not exist.")
-        return dir_list
+            print(f"\"JOB_DIR\" section does not exist.\n")
+        return dir_dict
 
+    # Read "ATOM_IDX" section
+    def read_atom_idx(self):
+        try:
+            atom_idx = self.config_ini["ATOM_IDX"]
+            idx_dict = dict(atom_idx)
+        except:
+            print(f"\"ATOM_IDX\" section does not exist.\n")
+        return idx_dict
 
-
+#    def read_params(self):
+#        atom_idx = self.config_ini["PARAMS"]["atom_idx"]
+#        return atom_idx
 
